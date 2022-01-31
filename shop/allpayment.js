1a434626a18fb2580fa5203fab4202ccb9e77a44
bless-botzz <blesstod38@gmail.com>
2022-01-31 07:50:10 +0700

    SCBOTBAYU

---
+++ shop/allpayment.js
@@ -0,0 +1,36 @@
+const allpayment = (prefix) => {
+return `┏━━━━━━━━━━━━━━━━━━━━
+┃                  *PAYMENT*
+┣━━━━━━━━━━━━━━━━━━━━
+┣━━━⊱❉ *🏧 BANK* ❉⊰━━━━✿
+┃  
+┣━⊱ *BRI*
+┣⊱ BRI [TIDAK TERSEDIA]
+┣━✿ NAMA BRI [TIDAK TERSEDIA]
+┗━━━━━━━━━━━━━━━━━━━━
+
+┏━━━━━━━━━━━━━━━━━━━━
+┃                  *PAYMENT*
+┣━━━━━━━━━━━━━━━━━━━━
+┣━━⊱❉ *💰 E-WALLET* ❉⊰━━━✿
+┃   
+┣━⊱ *GOPAY*
+┣⊱ -
+┣━━✿ NAMA GOPAY ${ownername}
+┃
+┣━⊱ *DANA*
+┣⊱ 0889 7565 5140
+┣━━✿ NAMA DANA ${ownername}
+┃
+┣━⊱ *PULSA*
+┣⊱ 0838 4532 9280
+┣━━✿ NAMA PULSA ${ownername}
+┃
+┗━━━━━━━━━━━━━━━━━━━━
+
+JANGAN LIAT² DOANG DONASI BENERAN DONG 🗿
+`
+	}
+
+exports.allpayment = allpayment
+ 
